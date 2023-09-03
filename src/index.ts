476255a1c93a2697cbf25effc7450002f3640476
caiotito <65377812+caiocalheiros@users.noreply.github.com>
2023-09-03 20:10:47 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-async function main() {
-    
-}
 
-main()
+(async () => {
+    const maes = await prisma.mae.create({
+        data: {
+            nome: "Luisa",
+            endereco: "Rua Dourada",
+            telefone: "4002-8922"
+        },
+    });
+    console.log(maes)
+})();
+
+/*(async () => {
+    const maes = await prisma.mae.count();
+    console.log(maes);
+})()*/
+
+
